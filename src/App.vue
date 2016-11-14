eaafe094a7b7c19a2d4813b5fdf58cbb8b41b2e3
Dave Honneffer <pearofducks@gmail.com>
2016-11-14 08:59:05 +0100

    Cleanup

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,7 @@
-<template>
-  <Layout />
-</template>
+<template> <Layout /> </template>
 
 <script>
 import Layout from './components/Layout'
 
 export default { components: { Layout } }
 </script>
-
-<style>
-</style>
